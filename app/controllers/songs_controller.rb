db5a66aa21d0aa8820cb55f7c825d95b1b4a6ee3
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-06-07 12:29:55 -0400

    read and models complete

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,15 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @artist = @song.artist
+    @genres = @song.genres
+
+    erb :'/songs/show'
+  end
+end
